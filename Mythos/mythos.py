c24af30ef45a622ac8c27a0ea5ccb13f20e9dcec
Anthony H <the7bunsoflife@gmail.com>
2018-10-19 20:57:38 -0400

    Add files via upload
    Added the groundwork for hero story generation

--- Mythos/mythos.py
+++ Mythos/mythos.py
@@ -1,18 +1,20 @@
-from random import *
+import random as rd
 from scriptos import * 
 
 
 
 def main():
     #Imports .txt files for the nouns and adjectives to determine god hood and stuff
-    nouns = file_open("data/allNouns.txt")
-    adj = file_open("data/allAdj.txt")
+    nouns = file_open("data/characters/nouns.txt")
+    adj = file_open("data/characters/adjectives.txt")
     pantheon = []
+    society = {}
     timeline = creation_myth()
     user_input = ""
 
     print("Welcome to Mythos! Mythology generation at your fingertips!")
-    print("[g] - generate gods, [c] - generate creation myth, [e] - exit")
+    print("[g] - generate gods, [c] - generate creation myth, [s] - society")
+    print("[h] - hero story, , [e] - exit")
 
     user_input = input("Enter in a command to get started: ")
 
@@ -21,15 +23,23 @@ def main():
         if user_input == 'g':
             pantheon = []
             for i in range(5):
-                pantheon.append(Character(gen_name(), gen_being(), randrange(0, 5999), choice(nouns), choice(adj), gen_power()))
+                pantheon.append(God(gen_name(), gen_being(), rd.randrange(0, 5999), rd.choice(nouns), rd.choice(adj), gen_power()))
 
             for i in pantheon:
                 i.printGod()
         
-        if user_input == 'c':
-            timeline = ""
-            timeline = creation_myth()
-            print(timeline)
+        elif user_input == 'c':
+            society["Creation myth"] = creation_myth()
+            print(society)
+
+        elif user_input == 's':
+            society = gen_society()
+            print(society)
+
+        elif user_input == 'h':
+            hero = hero_story(pantheon, society)
+            hero.print_myth()
+
 
         user_input = input("Enter another input: ")
 
--- Mythos/scriptos.py
+++ Mythos/scriptos.py
@@ -1,5 +1,6 @@
-from random import *
+import random as rd
 
+# Used to split .txt elements on a new line and append to a list
 def file_open(file_name):
     f = open(file_name, 'r')
     data = f.read().split("\n")
@@ -8,9 +9,42 @@ def file_open(file_name):
         return_list.append(i)
     return return_list
 
+adj = file_open("data/characters/adjectives.txt")
 
 
-adj = file_open("data/allAdj.txt")
+def gen_region():
+    climates = file_open("data/world/climates.txt")
+
+def gen_society():
+    cultures = file_open("data/world/cultures.txt")
+    society = {}
+    society['name'] = gen_name()
+    society['culture'] = rd.choice(cultures)
+
+    return society
+
+
+def hero_story(pantheon, society):
+    hero = Hero(gen_name(), "Hero", rd.choice(adj))
+    worshipped_god = rd.choice(pantheon)
+    villan_god = rd.choice(pantheon)
+    hero.society = society['name']
+
+    # This is currently the hacky way of making sure we don't end up with the same god
+    while(villan_god == worshipped_god):
+        villan_god = rd.choice(pantheon)
+
+    hero.hates = villan_god.name
+    hero.loves = worshipped_god.name
+
+    
+
+    return hero
+
+
+
+
+
 
 
 def creation_myth():
@@ -19,24 +53,24 @@ def creation_myth():
     #2 - Creation from chaos(expanse, void, abyss, disorder, primordial substance)
     #3 - World parent gods (Gods mating)
     #4 - World parent from a single god (The gods corpse or body gives life to the world)
-    timeline = []
-    rand = randrange(0, 4) 
+    timeline = ""
+    rand = rd.randrange(0, 4) 
     egg_birth_nouns = ["hatched", "birthed", "constructed"]
     egg_birth_objects = ["Egg", "Ball", "Skeleton"]
-    egg_origin_templates = ["Time began when our universe was", "The universe as know it was", "In nothingness our world was"]
+    egg_origin_templates = ["Time began when our universe was", "The universe as we know it was", "In nothingness our world was"]
     birth_obj = ""
 
     if rand == 0: 
-        birth_method = choice(egg_birth_nouns)
-        origin_method = choice(egg_origin_templates)
-        birth_obj = choice(adj) + ' '  + choice(egg_birth_objects)
-        timeline.append(f"{origin_method} {birth_method} from a {birth_obj}")
+        birth_method = rd.choice(egg_birth_nouns)
+        origin_method = rd.choice(egg_origin_templates)
+        birth_obj = rd.choice(adj) + ' '  + rd.choice(egg_birth_objects)
+        timeline += (f"{origin_method} {birth_method} from a {birth_obj}")
 
     elif rand == 1:
-        birth_method = choice(egg_birth_nouns)
-        origin_method = choice(egg_origin_templates)
+        birth_method = rd.choice(egg_birth_nouns)
+        origin_method = rd.choice(egg_origin_templates)
         birth_obj = "Empty"
-        timeline.append(f"{origin_method} {birth_method} from a {birth_obj}")
+        timeline += (f"{origin_method} {birth_method} from a {birth_obj}")
 
 
     return timeline
@@ -47,15 +81,15 @@ def creation_myth():
 def gen_name():
     name = ""
     words = ["que", "way", "eel", "ruu", "tish", "yar", "uub", "iek", "oy", "pe", "ah", "sha", "dor", "feek", "goo", "moo", "rabi", "doku", "baca", "szum", "lan", "ski", "-", "-"]
-    for i in range(randrange(2, 3)):
-        name += choice(words)
+    for i in range(rd.randrange(2, 3)):
+        name += rd.choice(words)
         
     return name
 
 def gen_power():
-    powers = file_open("data/allPowers.txt")
-    actions = file_open("data/allActions.txt")
-    the_power = choice(actions) + " " + choice(powers)
+    powers = file_open("data/characters/powers.txt")
+    actions = file_open("data/characters/actions.txt")
+    the_power = rd.choice(actions) + " " + rd.choice(powers)
 
     return the_power   
 
@@ -63,16 +97,16 @@ def gen_being():
     being = ""
     attributes = ["giant", "huge", "tiny", "", "meek", "strong"]
     beings = ["god", "goddess"]
-    being = choice(attributes) + " " + choice(beings)
+    being = rd.choice(attributes) + " " + rd.choice(beings)
     return being
 
 
 
 class Character:
-    def __init__(self, name, being, age, kind, adj, power):
+    def __init__(self, name, gender, age, kind, adj, power):
         self.name = name
         self.age = age
-        self.being = being #Is it a god, centaur, hero
+        self.gender = gender 
         self.kind = kind
         self.adj = adj
         self.power = power
@@ -91,12 +125,30 @@ class Character:
         self.children.append(child)
         child.setParent(self)
 
-    def printGod(self):
-        print(self.name, "the", self.adj, self.age, "year old", self.being, "of", self.kind, "has the power to", self.power)
-
     def printFamilyTree(self, depth):
         for child in self.children:
             for brace in range(depth):
                 print(" | ", end="")
             print(child.name)
             child.printFamilyTree(depth + 1)
+
+
+class God(Character): 
+    def printGod(self):
+        print(self.name, "the", self.adj, self.age, "year old", self.gender, "of", self.kind, "has the power to", self.power)
+
+class Hero(Character):
+    def __init__(self, name, gender, adj):
+            self.name = name
+            self.age = 0
+            self.gender = gender 
+            self.adj = adj
+            self.hates = ""
+            self.loves = ""
+            self.children = []
+            self.parents = []
+            self.story = ""
+            self.society = ""
+    
+    def print_myth(self):
+        print(self.name + " The mighty hero of " + self.society + " bested " +self.hates + " with help from " + self.loves) 
